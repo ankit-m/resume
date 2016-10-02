e6ef0add1174096fbd702a1eabb25e6b69b17ea7
Ankit Muchhala <muchhalaankit@gmail.com>
2016-10-02 05:37:01 +0530

    add data

---
+++ js/data.js
@@ -0,0 +1,61 @@
+var bio = {
+  name: 'Ankit Muchhala',
+  role: 'Web Developer',
+  contacts: {
+    mobile: '9924463029',
+    email: 'muchhalaankit@gmail.com',
+    github: 'ankit-m',
+    twitter: 'ankit_muchhala',
+    location: 'Gandhinagar'
+  },
+  welcomeMessage: 'Hello World',
+  skills: [''],
+  biopic: '',
+  display: function() {}
+};
+
+var education = {
+  schools: [{
+    name: 'DA-IICT',
+    location: 'Gandhinagar',
+    degree: 'B Tech',
+    majors: 'Information and Communication Technology',
+    dates: '',
+    url: ''
+  }],
+  onlineCourses: [{
+    title: 'Udacity Front-End Nanodegree',
+    school: 'Udacity',
+    dates: '',
+    url: ''
+  }],
+  display: function() {}
+};
+
+var experience = {
+  jobs: [{
+    employer: 'Google Summer of Code',
+    title: 'Web Developer',
+    location: 'Gandhinagar',
+    dates: '',
+    description: ''
+  }],
+  display: function() {}
+};
+
+var achievements = {
+  items: [{
+    description: '',
+    url: ''
+  }]
+};
+
+var projects = {
+  projects: [{
+    title: '',
+    dates: '',
+    description: '',
+    images: ''
+  }],
+  display: function() {}
+};
